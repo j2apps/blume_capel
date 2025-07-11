b3bf8f68542d6562e527323e6b6ee69c325c7190
Jonah Kim <j2.web.apps@gmail.com>
2025-07-11 10:52:45 -0500

    Reworked gap_size_statistics.cpp

--- corner_contribution/gap_size_analysis.py
+++ corner_contribution/gap_size_analysis.py
@@ -1,6 +1,7 @@
 import statistics
 import os
-def get_corner_contribtion(gap_size_statistics, num_samples, L, l):
+def get_corner_contribution(gap_size_statistics, num_samples, L, l):
+    # Apply corner contribution formula
     acc = 0
     for i in range(0, l):
         for j in range(i, L//2):
@@ -8,6 +9,7 @@ def get_corner_contribtion(gap_size_statistics, num_samples, L, l):
     return (1/2) * (1/(L*L)) * (1/num_samples) * acc
 
 def get_gap_array(filename):
+    # Get an array of numbers from the text file
     gap_size_statistics = []
     with open(filename, 'r') as file:
         lines = file.readlines()
@@ -19,15 +21,18 @@ def get_gap_array(filename):
 
 if __name__ == "__main__":
     root = "./corner_contribution/data/ising/spin"
-
+    # Iterate through each L value
     for l in (8, 16, 32, 64, 128):
         corner_contributions = list()
+        # Find all files in the directory
         with os.scandir(f"{root}/{l}") as entries:
             for entry in entries:
+                # Get the stats and number of samples from each file
                 gap_size_statistics, num_samples = get_gap_array(entry)
-                # print(gap_size_statistics, num_samples)
-                corner_contribution = get_corner_contribtion(gap_size_statistics, num_samples, l, l//2)
+                # Get the corner contribution and append
+                corner_contribution = get_corner_contribution(gap_size_statistics, num_samples, l, l//2)
                 corner_contributions.append(corner_contribution)
+        # Calculate the mean and SE of the corner contribution
         mean_corner_contribution = statistics.mean(corner_contributions)
         stdev_corner_contribution = statistics.stdev(corner_contributions)
         print(l, mean_corner_contribution, stdev_corner_contribution)
