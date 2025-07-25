64dd77754f2dc0bce506c8bd8b574cc72361161b
j2apps <jonah.kim.2024@gmail.com>
2025-07-25 15:36:02 -0500

    configured job scripts

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 module purge all
 module load gcc/12.3.0-gcc
 
-for l in 8 16;
+for l in 16 32 64;
 do
-	g++ ./main.cpp -std=c++20 -fopenmp -DL_MACRO=$l -DNUM_THREADS=1 -O3 -o "./compiled/ising_run_"$l
+	g++ ./main.cpp -std=c++20 -fopenmp -DL_MACRO=$l -DNUM_THREADS=2 -O3 -o "./compiled/ising_brun"$l
 done
 
 
--- job_scripts/run.sh
+++ job_scripts/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --account=b1140
-#SBATCH --array=0-199
-#SBATCH --partition=b1140
+#SBATCH --account=p32813
+#SBATCH --array=0-299
+#SBATCH --partition=short
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH -t 00:02:00
+#SBATCH --cpus-per-task=2
+#SBATCH -t 4:00:00
 #SBATCH --mem=10M
 #SBATCH --job-name="smallising"
 #SBATCH --output=outlog
@@ -17,6 +17,6 @@ module load gcc/12.3.0-gcc
 
 run=$((${SLURM_ARRAY_TASK_ID} % 100))
 size=$((SLURM_ARRAY_TASK_ID / 100))
-declare -a l=(8 16)
+declare -a l=(16 32 64)
 
-./compiled/ising_${l[$size]} $run ./data/ising-7-24 ./data/burn-7-23/ising/${l[$size]}_burn.txt
+./compiled/ising_brun${l[$size]} $run ./data/ising-7-24 ./data/burn-7-23/ising/${l[$size]}_burn.txt
