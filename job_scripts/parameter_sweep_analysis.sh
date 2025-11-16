825e6833ec05045fa3f43dd44aa57441d4aa8ac8
j2apps <jonah.kim.2024@gmail.com>
2025-11-16 12:38:58 -0600

    job script update

--- job_scripts/parameter_sweep_analysis.sh
+++ job_scripts/parameter_sweep_analysis.sh
@@ -1,8 +1,8 @@
-i=4
-for j in {1..3};
+ti=1
+for di in 2 3 4;
 do
-	sbatch ./job_scripts/gap_size.sh "./data/sweep/t${i}_d${j}"
-	sbatch ./job_scripts/gamma_nu.sh "./data/sweep/t${i}_d${j}"
+	sbatch ./job_scripts/gap_size.sh "./data/sweep/t${ti}_d${di}"
+	sbatch ./job_scripts/gamma_nu.sh "./data/sweep/t${ti}_d${di}"
 done
 		
 
